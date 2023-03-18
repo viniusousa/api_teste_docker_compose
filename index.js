cffc0f9a148159cc88b06c1ca2ebc192fab2e11d
viniusousa <viniciussmit@gmail.com>
2023-03-18 17:38:09 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+const PORT = 3000;
+
+
+app.get('/', (req, res) => {
+    res.json('Hello World! nodemon ');
+    }
+);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    }
+);
